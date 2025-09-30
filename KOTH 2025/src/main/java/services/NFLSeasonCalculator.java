18e5256d2a545af887f56009ddd9d40d691e6035
MikeGriffin1959 <griff@griffin.org>
2025-09-30 19:15:34 -0400

    Bug 755

--- KOTH 2025/src/main/java/services/NFLSeasonCalculator.java
+++ KOTH 2025/src/main/java/services/NFLSeasonCalculator.java
@@ -79,7 +79,7 @@ public class NFLSeasonCalculator {
 
     private LocalDateTime getSeasonStartDateTime(int year) {
         LocalDate seasonStartDate = getSeasonStartDate(year);
-        LocalDateTime seasonStartDateTime = seasonStartDate.atTime(6, 0); // 6 AM on season start date
+        LocalDateTime seasonStartDateTime = seasonStartDate.atTime(14, 0); // 2PM on season start date
         // Adjust to previous Tuesday 6 AM
         while (seasonStartDateTime.getDayOfWeek() != DayOfWeek.TUESDAY) {
             seasonStartDateTime = seasonStartDateTime.minusDays(1);
